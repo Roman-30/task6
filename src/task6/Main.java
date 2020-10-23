f54151c65a489d21f4e7fed4756799a3874c792a
Roman Shevtsov <Romses2016@yandex.ru>
2020-10-23 19:22:06 +0300

    task6

---
+++ src/task6/Main.java
@@ -0,0 +1,51 @@
+package task6;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = reaSequenceElements("Enter 'X' value: ");
+        int n = reaSequenceElements("Enter the number of members in the sequence to sum them. n = ");
+        int repeat  = calculateNumberRepetitions(n);
+        int summa = calculateSumSequence(x, repeat);
+
+        printSum(n, summa);
+    }
+
+    public static int reaSequenceElements(String name) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(name);
+        return in.nextInt();
+    }
+
+    public static int calculateSumSequence(int x, int repeat) {
+        int x0 = x;
+        int sum = 0;
+
+        for (int i = 1; i <= repeat; i++) {
+            double v = ((i + 2) * (i + 1)) / 2 * Math.pow(x0, i);
+            if (checkCycleNumber(i)) {
+                sum = (int) (x + v);
+                // System.out.println(i);
+            } else {
+                sum = (int) (x - v);
+                // System.out.println(i);
+            }
+            x = sum;
+        }
+        return sum;
+    }
+
+    public static int calculateNumberRepetitions(int n) {
+        return n * 2;
+    }
+
+    public static boolean checkCycleNumber(int i) {
+        return i % 2 != 1;
+    }
+
+    public static void printSum(int n, int summa) {
+        System.out.print("Sum of the first " + n + " members of the sequence = " + summa);
+    }
+}
